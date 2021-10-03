1ffd003a95f3d624e6cd6b04b6ac7b26fae7e8e0
alkadafare01 <pinki.dafare@gmail.com>
2021-10-03 22:22:23 +0530

    stage 2 added

--- rough/test.py
+++ rough/test.py
@@ -1,4 +1,9 @@
+import os
+
 def main():
-    print("enter")
+    print(">>>> ALWAYS RUN >>>>>>")
+    with open(os.path.join("rough", "test.txt"), "w") as f:
+        f.write("This is a test file!!")
+
 if __name__ == '__main__':
     main()
